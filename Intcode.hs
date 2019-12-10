7d58bb6ffb2c12fd74c153e320531462cac0622b
Kyle Parsons <parsons.kyle.89@gmail.com>
2019-12-09 20:44:55 -0500

    refactor Intcode.hs

--- Intcode.hs
+++ Intcode.hs
@@ -1,28 +1,29 @@
 module Intcode (runProgn, readProgn) where
 
+import System.IO (hFlush, stdout)
 import System.Exit (exitSuccess, exitFailure)
 import qualified Data.Vector as V
 import Data.List.Split (splitOn)
 
-type Progn = V.Vector Int
+data Progn = Progn (V.Vector Int) deriving (Show)
 type Ptr = Int
 type Instruction = Ptr -> ParameterModes -> Progn -> IO (Progn, Int)
 data ParameterMode = PositionMode | ImmediateMode
 type ParameterModes = [ParameterMode]
 
 setValue :: Progn -> Ptr -> Int -> Progn
-setValue progn ptr val = progn V.// [(ptr, val)]
+setValue (Progn code) ptr val = Progn (code V.// [(ptr, val)])
 
 (!?) :: ParameterModes -> Int -> ParameterMode
 (!?) parameterModes idx = 
-  if idx < length parameterModes
-  then parameterModes !! idx
+  if idx <= length parameterModes
+  then parameterModes !! (idx - 1)
   else PositionMode
 
 readProgn :: String -> IO Progn
 readProgn fileName = do
   fileContents <- readFile fileName
-  return $ V.fromList $ map read (splitOn [','] fileContents)
+  return $ Progn (V.fromList (map read (splitOn [','] fileContents)))
 
 runProgn :: Progn -> IO Progn
 runProgn = evaluate 0
@@ -32,14 +33,21 @@ evaluate ptr progn = do
   let (instructionCode, parameterModes) = parseOpCode progn ptr
   let instruction = getInstruction instructionCode
   (newProgn, newPtr) <- instruction ptr parameterModes progn
-  if instructionCode /= 99
-  then evaluate newPtr newProgn
-  else return newProgn
+  if instructionCode /= 99 then evaluate newPtr newProgn else return newProgn
 
+
+getParam :: ParameterModes -> Progn -> Ptr -> Int -> Int
+getParam parameterModes progn ptr idx = let
+  paramptr = getPtr progn (ptr + idx)
+  parameterMode = parameterModes !? idx in
+  deref parameterMode progn paramptr
+
+getPtr :: Progn -> Ptr -> Ptr
+getPtr = deref PositionMode
 
 deref :: ParameterMode -> Progn -> Ptr -> Int
 deref ImmediateMode _ ptr = ptr
-deref PositionMode progn ptr = progn V.! ptr
+deref PositionMode (Progn code) ptr = code V.! ptr
 
 parseOpCode :: Progn -> Int -> (Int, ParameterModes)
 parseOpCode progn ptr = let
@@ -57,83 +65,77 @@ parseParameterMode '0' = PositionMode
 parseParameterMode '1' = ImmediateMode
 
 getInstruction :: Int -> Instruction
-getInstruction 99 = exitInstruction
-getInstruction 1 = addInstruction
-getInstruction 2 = multiplyInstruction
-getInstruction 3 = inputInstruction
-getInstruction 4 = outputInstruction
-getInstruction 5 = jumpIfTrueInstruction
-getInstruction 6 = jumpIfFalseInstruction
-getInstruction 7 = lessThanTestInstruction
-getInstruction 8 = equalsTestInstruction
+getInstruction 99 = exit
+getInstruction 1 = add
+getInstruction 2 = multiply
+getInstruction 3 = input
+getInstruction 4 = output
+getInstruction 5 = jumpIfTrue
+getInstruction 6 = jumpIfFalse
+getInstruction 7 = lessThanTest
+getInstruction 8 = equalsTest
 getInstruction _ = failInstruction
 
 operInstruction :: (Int -> Int -> Int) -> Instruction
 operInstruction oper ptr parameterModes progn = let
-  ptr1 = progn V.! (ptr + 1)
-  ptr2 = progn V.! (ptr + 2)
-  outptr = progn V.! (ptr + 3)
-  inval1 = deref (parameterModes !? 0) progn ptr1
-  inval2 = deref (parameterModes !? 1) progn ptr2
+  inval1 = getParam parameterModes progn ptr 1
+  inval2 = getParam parameterModes progn ptr 2
+  outptr = getPtr progn (ptr + 3)
   outval = inval1 `oper` inval2 in
   return (setValue progn outptr outval, ptr + 4)
 
-addInstruction :: Instruction
-addInstruction = operInstruction (+)
+add :: Instruction
+add = operInstruction (+)
 
-multiplyInstruction :: Instruction
-multiplyInstruction = operInstruction (*)
+multiply :: Instruction
+multiply = operInstruction (*)
 
-exitInstruction ::  Instruction
-exitInstruction ptr _ progn = return (progn, ptr + 1)
+exit ::  Instruction
+exit ptr _ progn = return (progn, ptr + 1)
 
 failInstruction :: Instruction
 failInstruction _ _ progn = exitFailure
 
-inputInstruction :: Instruction
-inputInstruction ptr _ progn = do
+input :: Instruction
+input ptr _ progn = do
   putStr "Input an Int: "
+  hFlush stdout
   input <- readInt
-  let outreg = progn V.! (ptr + 1)
+  let outreg = getPtr progn (ptr + 1)
   return (setValue progn outreg input, ptr + 2)
 
 readInt :: IO Int
 readInt = getLine >>= return . read
 
-outputInstruction :: Instruction
-outputInstruction ptr parameterModes progn = do
-  let outptr = progn V.! (ptr + 1)
-  let outval = deref (parameterModes !? 0) progn outptr
+output :: Instruction
+output ptr parameterModes progn = do
+  let outval = getParam parameterModes progn ptr 1
   print outval
   return (progn, ptr + 2)
 
 jumpIfTestInstruction :: (Int -> Bool) -> Instruction
 jumpIfTestInstruction predicate ptr parameterModes progn = let
-  testptr = progn V.! (ptr + 1)
-  outptrptr = progn V.! (ptr + 2)
-  testval = deref (parameterModes !? 0) progn testptr
-  outptrval = deref (parameterModes !? 1) progn outptrptr
+  testval = getParam parameterModes progn ptr 1
+  outptrval = getParam parameterModes progn ptr 2
   newptr = if predicate testval then outptrval else (ptr + 3) in
   return (progn, newptr)
 
-jumpIfTrueInstruction :: Instruction
-jumpIfTrueInstruction = jumpIfTestInstruction (/=0)
+jumpIfTrue :: Instruction
+jumpIfTrue = jumpIfTestInstruction (/=0)
 
-jumpIfFalseInstruction :: Instruction
-jumpIfFalseInstruction = jumpIfTestInstruction (==0)
+jumpIfFalse :: Instruction
+jumpIfFalse = jumpIfTestInstruction (==0)
 
 testInstruction :: (Int -> Int -> Bool) -> Instruction
 testInstruction bipredicate ptr parameterModes progn = let
-  ptr1 = progn V.! (ptr + 1)
-  ptr2 = progn V.! (ptr + 2)
-  outptr = progn V.! (ptr + 3)
-  val1 = deref (parameterModes !? 0) progn ptr1
-  val2 = deref (parameterModes !? 1) progn ptr2
+  val1 = getParam parameterModes progn ptr 1
+  val2 = getParam parameterModes progn ptr 2
+  outptr = getPtr progn (ptr + 3)
   outval = if bipredicate val1 val2 then 1 else 0 in
   return (setValue progn outptr outval, ptr + 4)
 
-lessThanTestInstruction :: Instruction
-lessThanTestInstruction = testInstruction (<)
+lessThanTest :: Instruction
+lessThanTest = testInstruction (<)
 
-equalsTestInstruction :: Instruction
-equalsTestInstruction = testInstruction (==)
+equalsTest :: Instruction
+equalsTest = testInstruction (==)
