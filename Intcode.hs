c22f178726f968c88e9d480f3c1b2845e5c61c36
Kyle Parsons <parsons.kyle.89@gmail.com>
2019-12-11 23:59:57 -0500

    total refactoring of intcode computer

--- Intcode.hs
+++ Intcode.hs
@@ -1,141 +1,135 @@
-module Intcode (runProgn, readProgn) where
+module Intcode (PrognState (PrognState), readPrognState, executePrognIO) where
 
+import System.Exit
 import System.IO (hFlush, stdout)
-import System.Exit (exitSuccess, exitFailure)
 import qualified Data.Vector as V
 import Data.List.Split (splitOn)
+import Data.Tuple (swap)
 
-data Progn = Progn (V.Vector Int) deriving (Show)
 type Ptr = Int
-type Instruction = Ptr -> ParameterModes -> Progn -> IO (Progn, Int)
-data ParameterMode = PositionMode | ImmediateMode
-type ParameterModes = [ParameterMode]
 
-setValue :: Progn -> Ptr -> Int -> Progn
-setValue (Progn code) ptr val = Progn (code V.// [(ptr, val)])
+data PrognState = PrognState Memory Ptr deriving (Show)
+type Memory = V.Vector Int
 
-(!?) :: ParameterModes -> Int -> ParameterMode
-(!?) parameterModes idx = 
-  if idx <= length parameterModes
-  then parameterModes !! (idx - 1)
-  else PositionMode
+data Param = PositionMode | ImmediateMode deriving (Show)
 
-readProgn :: String -> IO Progn
-readProgn fileName = do
+type OpCodeEncoding = Int
+type ParamModeEncodings = Int
+
+data Operation = Exit
+               | Add Param Param Param
+               | Multiply Param Param Param
+               | Input Param
+               | Output Param
+               | JumpIfTrue Param Param
+               | JumpIfFalse Param Param
+               | TestLessThan Param Param Param
+               | TestEqual Param Param Param
+               | ParseFailure deriving (Show)
+
+isExit :: Operation -> Bool
+isExit Exit = True
+isExit _ = False
+
+listToPrognState :: [Int] -> PrognState
+listToPrognState memory = PrognState (V.fromList memory) 0
+
+readMemory :: String -> IO Memory
+readMemory fileName = do
   fileContents <- readFile fileName
-  return $ Progn (V.fromList (map read (splitOn [','] fileContents)))
+  return $ V.fromList (map read (splitOn [','] fileContents))
 
-runProgn :: Progn -> IO Progn
-runProgn = evaluate 0
+readPrognState :: String -> IO PrognState
+readPrognState fileName = do
+  memory <- readMemory fileName
+  return (PrognState memory 0)
 
-evaluate :: Ptr -> Progn -> IO Progn
-evaluate ptr progn = do
-  let (instructionCode, parameterModes) = parseOpCode progn ptr
-  let instruction = getInstruction instructionCode
-  (newProgn, newPtr) <- instruction ptr parameterModes progn
-  if instructionCode /= 99 then evaluate newPtr newProgn else return newProgn
+executePrognIO :: PrognState -> IO PrognState
+executePrognIO prognState = do
+  let oper = semanticAnalysis prognState
+  if isExit oper
+  then return prognState
+  else executeOperIO oper prognState >>= executePrognIO
 
-
-getParam :: ParameterModes -> Progn -> Ptr -> Int -> Int
-getParam parameterModes progn ptr idx = let
-  paramptr = getPtr progn (ptr + idx)
-  parameterMode = parameterModes !? idx in
-  deref parameterMode progn paramptr
-
-getPtr :: Progn -> Ptr -> Ptr
-getPtr = deref PositionMode
-
-deref :: ParameterMode -> Progn -> Ptr -> Int
-deref ImmediateMode _ ptr = ptr
-deref PositionMode (Progn code) ptr = code V.! ptr
-
-parseOpCode :: Progn -> Int -> (Int, ParameterModes)
-parseOpCode progn ptr = let
-  rawOpCode = deref PositionMode progn ptr
-  (parameterModesInt, opCode) = rawOpCode `divMod` 100
-  parameterModes = parseParameterModes parameterModesInt in
-  (opCode, parameterModes)
-
-parseParameterModes :: Int -> ParameterModes
-parseParameterModes parameterModesInt =
-  map parseParameterMode $ reverse (show parameterModesInt)
-
-parseParameterMode :: Char -> ParameterMode
-parseParameterMode '0' = PositionMode
-parseParameterMode '1' = ImmediateMode
-
-getInstruction :: Int -> Instruction
-getInstruction 99 = exit
-getInstruction 1 = add
-getInstruction 2 = multiply
-getInstruction 3 = input
-getInstruction 4 = output
-getInstruction 5 = jumpIfTrue
-getInstruction 6 = jumpIfFalse
-getInstruction 7 = lessThanTest
-getInstruction 8 = equalsTest
-getInstruction _ = failInstruction
-
-operInstruction :: (Int -> Int -> Int) -> Instruction
-operInstruction oper ptr parameterModes progn = let
-  inval1 = getParam parameterModes progn ptr 1
-  inval2 = getParam parameterModes progn ptr 2
-  outptr = getPtr progn (ptr + 3)
-  outval = inval1 `oper` inval2 in
-  return (setValue progn outptr outval, ptr + 4)
-
-add :: Instruction
-add = operInstruction (+)
-
-multiply :: Instruction
-multiply = operInstruction (*)
-
-exit ::  Instruction
-exit ptr _ progn = return (progn, ptr + 1)
-
-failInstruction :: Instruction
-failInstruction _ _ progn = exitFailure
-
-input :: Instruction
-input ptr _ progn = do
+executeOperIO :: Operation -> PrognState -> IO PrognState
+executeOperIO (Input param) (PrognState memory ptr) = do
   putStr "Input an Int: "
   hFlush stdout
-  input <- readInt
-  let outreg = getPtr progn (ptr + 1)
-  return (setValue progn outreg input, ptr + 2)
+  val <- readInt
+  let regOut = deref memory param (ptr + 1)
+  let newMemory = set memory regOut val
+  let newPtr = (ptr + 2)
+  return $ PrognState newMemory newPtr
+executeOperIO (Output param) (PrognState memory ptr) = do
+  putStrLn . show $ deref memory param (ptr + 1)
+  return $ PrognState memory (ptr + 2)
+executeOperIO oper prognState = return $ executeOper oper prognState
+
+executeOper :: Operation -> PrognState -> PrognState
+executeOper (Add left right out) prognState = executeBinaryOper (+) left right out prognState
+executeOper (Multiply left right out) prognState = executeBinaryOper (*) left right out prognState
+executeOper (JumpIfTrue test to) prognState = executeJumpOper (/=0) test to prognState
+executeOper (JumpIfFalse test to) prognState = executeJumpOper (==0) test to prognState
+executeOper (TestLessThan left right out) prognState = executeBinaryOper (\l r -> fromEnum (l < r)) left right out prognState
+executeOper (TestEqual left right out) prognState = executeBinaryOper (\l r -> fromEnum (l == r)) left right out prognState
+
+executeBinaryOper :: (Int -> Int -> Int) -> Param -> Param -> Param -> PrognState -> PrognState
+executeBinaryOper intOper left right out (PrognState memory ptr) = let
+  valLeft = deref memory left (ptr + 1)
+  valRight = deref memory right (ptr + 2)
+  regOut = deref memory out (ptr + 3)
+  valOut = valLeft `intOper` valRight
+  newMemory = set memory regOut valOut
+  newPtr = ptr + 4 in
+  PrognState newMemory newPtr
+
+executeJumpOper :: (Int -> Bool) -> Param -> Param -> PrognState -> PrognState
+executeJumpOper pred test to (PrognState memory ptr) = let
+  valTest = deref memory test (ptr + 1)
+  newPtr = if pred valTest then deref memory to (ptr + 2) else (ptr + 3) in
+  (PrognState memory newPtr)
 
 readInt :: IO Int
 readInt = getLine >>= return . read
 
-output :: Instruction
-output ptr parameterModes progn = do
-  let outval = getParam parameterModes progn ptr 1
-  print outval
-  return (progn, ptr + 2)
+deref :: Memory -> Param -> Ptr -> Int
+deref memory ImmediateMode ptr = get memory ptr
+deref memory PositionMode ptr = let
+  newPtr = deref memory ImmediateMode ptr in
+  deref memory ImmediateMode newPtr
 
-jumpIfTestInstruction :: (Int -> Bool) -> Instruction
-jumpIfTestInstruction predicate ptr parameterModes progn = let
-  testval = getParam parameterModes progn ptr 1
-  outptrval = getParam parameterModes progn ptr 2
-  newptr = if predicate testval then outptrval else (ptr + 3) in
-  return (progn, newptr)
+get :: Memory -> Ptr -> Int
+get = (V.!)
 
-jumpIfTrue :: Instruction
-jumpIfTrue = jumpIfTestInstruction (/=0)
+set :: Memory -> Ptr -> Int -> Memory
+set memory ptr val = memory V.// [(ptr, val)]
 
-jumpIfFalse :: Instruction
-jumpIfFalse = jumpIfTestInstruction (==0)
+semanticAnalysis :: PrognState -> Operation
+semanticAnalysis (PrognState memory ptr) = let
+  rawOpCode = (get memory ptr)
+  (opCode, paramModes) = parseRawOpCode rawOpCode in
+  generateOperation opCode paramModes
 
-testInstruction :: (Int -> Int -> Bool) -> Instruction
-testInstruction bipredicate ptr parameterModes progn = let
-  val1 = getParam parameterModes progn ptr 1
-  val2 = getParam parameterModes progn ptr 2
-  outptr = getPtr progn (ptr + 3)
-  outval = if bipredicate val1 val2 then 1 else 0 in
-  return (setValue progn outptr outval, ptr + 4)
+parseRawOpCode :: Int -> (OpCodeEncoding, ParamModeEncodings)
+parseRawOpCode rawOpCode = swap (rawOpCode `divMod` 100)
 
-lessThanTest :: Instruction
-lessThanTest = testInstruction (<)
+generateOperation :: OpCodeEncoding -> ParamModeEncodings -> Operation
+generateOperation 99 _ = Exit
+generateOperation 1 paramModes = Add (paramModes !? 1) (paramModes !? 2) ImmediateMode
+generateOperation 2 paramModes = Multiply (paramModes !? 1) (paramModes !? 2) ImmediateMode
+generateOperation 3 _ = Input ImmediateMode
+generateOperation 4 paramModes = Output (paramModes !? 1)
+generateOperation 5 paramModes = JumpIfTrue (paramModes !? 1) (paramModes !? 2)
+generateOperation 6 paramModes = JumpIfFalse (paramModes !? 1) (paramModes !? 2)
+generateOperation 7 paramModes = TestLessThan (paramModes !? 1) (paramModes !? 2) ImmediateMode
+generateOperation 8 paramModes = TestEqual (paramModes !? 1) (paramModes !? 2) ImmediateMode
 
-equalsTest :: Instruction
-equalsTest = testInstruction (==)
+(!?) :: ParamModeEncodings -> Int -> Param
+(!?) paramModes idx = decodeParamMode (parameterModeN paramModes idx)
+
+parameterModeN :: ParamModeEncodings -> Int -> Int
+parameterModeN paramModes idx = paramModes `div` (10^(idx-1)) `mod` 10
+
+decodeParamMode :: Int -> Param
+decodeParamMode 0 = PositionMode
+decodeParamMode 1 = ImmediateMode
