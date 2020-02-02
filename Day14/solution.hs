2dc8c62fa7ba8c477db85fd83dba4034fc230605
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-02-02 13:44:47 -0500

    first pass on solution

--- Day14/solution.hs
+++ Day14/solution.hs
@@ -3,19 +3,22 @@ import System.Environment
 import System.Exit
 import qualified Data.Map.Strict as Map
 import Data.Char (isSpace)
-import Data.List (dropWhileEnd)
+import Data.Function (on)
+import Data.Ord (comparing)
+import Data.List (dropWhileEnd, sortBy, groupBy)
 import Data.List.Split (splitOn)
 
 type Element = String
-data Ingredient = Ingredient { amount :: Int, element :: Element } deriving (Show)
+data Ingredient = Ingredient { amount :: Int, element :: Element } deriving (Eq, Show)
 data Formula = Formula { to :: Ingredient, from :: [Ingredient] } deriving (Show)
 type Table = Map.Map Element Formula
 
 main = do
   fileName <- getArgs >>= parseArgs
   contents <- readFile fileName
-  let conversions = parseContents contents
-  print conversions
+  let rawConversions = parseContents contents
+  let conversions = Map.insert "ORE" (Formula (Ingredient 1 "ORE") [(Ingredient 1 "ORE")]) rawConversions
+  print $ transformAll conversions [(Ingredient 1 "FUEL")]
 
 parseArgs :: [String] -> IO String
 parseArgs [fileName] = return $ fileName
@@ -38,8 +41,47 @@ splitLine line = let
 
 parseIngredient :: String -> Ingredient
 parseIngredient ingredient = let
-  (amount, element) = break isSpace $ dropWhile isSpace $ dropWhileEnd isSpace ingredient in
+  (amount, ' ':element) = break isSpace $ dropWhile isSpace $ dropWhileEnd isSpace ingredient in
   Ingredient (read amount) element
 
 parseFrom :: String -> [Ingredient]
 parseFrom from = map parseIngredient (splitOn "," from) 
+
+transformAll :: Table -> [Ingredient] -> [Ingredient]
+transformAll conversions froms = let
+  transformer = simplify . (transformOnce conversions) in
+  until (idempotent transformer) transformer froms
+
+transformOnce :: Table -> [Ingredient] -> [Ingredient]
+transformOnce conversions froms = concatMap (transform conversions) froms
+
+transform :: Table -> Ingredient -> [Ingredient]
+transform conversions (Ingredient neededAmount neededElement) = let
+  Just (Formula (Ingredient producedAmount _) ingredients) = Map.lookup neededElement conversions
+  numberReactions = neededAmount `divUp` producedAmount 
+  leftOverNeeded = numberReactions * producedAmount - neededAmount
+  fromIngredients = multiplyIngredients numberReactions ingredients in
+  if leftOverNeeded == 0
+  then fromIngredients
+  else (Ingredient leftOverNeeded neededElement) : fromIngredients
+
+multiplyIngredients :: Int -> [Ingredient] -> [Ingredient]
+multiplyIngredients factor ingredients = map (multiplyIngredient factor) ingredients
+
+multiplyIngredient :: Int -> Ingredient -> Ingredient
+multiplyIngredient factor (Ingredient amount element) = Ingredient (factor * amount) element
+
+idempotent :: (Eq a) => (a -> a) -> a -> Bool
+idempotent f = \x -> x == f x
+
+divUp :: Int -> Int -> Int
+divUp num denom = -((-num) `div` denom)
+
+simplify :: [Ingredient] -> [Ingredient]
+simplify ingredients = map sumIngredient (groupIngredients ingredients)
+
+sumIngredient :: [Ingredient] -> Ingredient
+sumIngredient ingredients = foldl1 (\(Ingredient a e) (Ingredient b _) -> (Ingredient (a + b) e)) ingredients
+
+groupIngredients :: [Ingredient] -> [[Ingredient]]
+groupIngredients ingredients = groupBy ((==) `on` element) $ sortBy (comparing element) ingredients
